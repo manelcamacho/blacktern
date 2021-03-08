ddcd3bfa5d6c8db29adb299f14ccf39cfb2e96ac
manelcamacho <45488545+manelcamacho@users.noreply.github.com>
2021-03-08 00:24:35 +0000

    Black tern 0.2
    This version includes the velocity calculation of transitional water waves and a simple algorithm to divide the linear deep water waves from the linear transitional water waves. The user can also define where its data file is located, this adding the <path to file/name of file.txt> after running the program. As usual to create an executable you just need to run in your unix/osx/linux terminal gcc blacktern.c -o blacktern.
    
    To run the program, go to the folder were blacktern is stored and type ./blacktern <path to the file with the wave data>.txt
    
    The wave data must be given as explained in the "how to use" text file. See you in 0.3 really soon!.

--- blacktern.c
+++ blacktern.c
@@ -3,23 +3,25 @@
 #include<math.h>
 #include<unistd.h>
 #include<stdlib.h>
+#define BUFSIZE 1000
 
 float typeofwave(float Lw, float h);
 float wavelenght(float *Tw, float h, float lat);
 float planetarylocalgravity(float lat);
-int read_ints(void);
+int read_ints();
 
 int velocitiesxd(float *ampa, float *dx, float *dz, float *dt, float *Twa, float Lwa, float h, int in);
+int velocitiesxt(float *ampa, float *dx, float *dz, float *dt, float *Twa, float Lwa, float h, int in);
 
 
 
 float pi=3.14159;
 float conv=1;
 
-int main(void)
+int main(int argc, char *argv[])
 {
 float Lwa2=0, g=9.81, a=0, m=0.02615, mt=0, xt=0,yt=0, op=0, dif=0, amp=0, lat=0;
-int condition=0, i=0, t=0, h=0;
+int condition=0, i=0, t=0, h=0, Lw=0;
 
 //we read the size of the data file to store infromation on the arrays
 t=read_ints();
@@ -63,8 +65,9 @@ if (!Cwa) {
 }
 
 //Here we store the data from the file to each array as it corresponds
-FILE *fpa;
-fpa=fopen("data.txt", "r");
+FILE *fpa = fopen(argv[1], "r"); /* "r" = open for reading */
+char buff[BUFSIZE]; /* a buffer to hold what you read in */
+
 int count=1, ip=0, ia=0, ix=0, iz=0, it=0, condit=1;
 
 while(!feof (fpa))
@@ -133,19 +136,22 @@ printf("\n" );
 
 
 
-while (i<=t){
-xt=(h/(g*pow(Twa[i],2)));
-yt=(ampa[i]/(g*pow(Twa[i],2)));
-mt=(yt-.00005)/(xt-.001369);
+while (i<=t)
+{
+  //The non-dimensional parameters to calculate the wave order are calculated
+  xt=(h/(g*pow(Twa[i],2)));
+  yt=(ampa[i]/(g*pow(Twa[i],2)));
+  mt=(yt-.00005)/(xt-.001369);
 
-if (mt<m||((yt<.001)&&(xt>0.37)))
-{
-op=1;
-}
-else
-{
-  op=2;
-}
+//Here we define if our wave is linear or 2nd order
+  if (mt<m||((yt<.001)&&(xt>0.37)))
+    {
+      op=1;
+    }
+    else
+    {
+      op=2;
+    }
 
 Lwa2=wavelenght(Twa+i,h,lat);
 Cwa[i]=typeofwave(Lwa2,h);
@@ -162,13 +168,21 @@ if (Cwa[i]==1&&op==1)
 
 
 }
-else if (Cwa[i]==2&&op==1)
+else if (Cwa[i]==3&&op==1)
 {
-    //Case for linear shallow water waves
+  dif=1;
+  Lwa2=1.56*pow(Twa[i],2);
+  while(dif>0.1)
+  {
+  Lw=1.56*pow(Twa[i],2)*tanh((2*pi*h)/Lwa2);
+  Lwa2=Lw;
+  dif=sqrt(pow(pow(Lw,2)-pow(Lwa2,2),2));
+  }
+  velocitiesxt(ampa+i, dx+i, dz+i, dt+i, Twa+i, Lwa2, h, i);
 
 }
 
-else if(Cwa[i]==3&&op==1)
+else if(Cwa[i]==2&&op==1)
 {
     //Case for linear transitional water waves
 }
@@ -239,13 +253,12 @@ int read_ints (void)
 {
    FILE *fp;
   fp=fopen("data.txt", "r");
-  int i = 0, count=1;
+  float i = 0, count=1;
 
-  fscanf(fp, "%d", &i);
+  fscanf(fp, "%f", &i);
   while(!feof (fp))
     {
-      printf("%d ", i);
-      fscanf(fp, "%d", &i);
+      fscanf(fp, "%f", &i);
       count++;
     }
   fclose(fp);
@@ -301,6 +314,7 @@ int velocitiesxd(float *ampa, float *dx, float *dz, float *dt, float *Twa, float
                                     arrayx[i]=0;
                                     arrayy[i]=0;
 
+
                                   }
                                 else
                                   {
@@ -313,7 +327,7 @@ int velocitiesxd(float *ampa, float *dx, float *dz, float *dt, float *Twa, float
                                   x=x+(*dx);
                                 }
                                 while(i2<=i){
-                                  //We store the whole data from the arrays to a file 
+                                  //We store the whole data from the arrays to a file
                                   fprintf(fp, "%.3f,%.3f\t", arrayx[i2], arrayy[i2]);
                                   i2++;
                                 }
@@ -330,4 +344,77 @@ int velocitiesxd(float *ampa, float *dx, float *dz, float *dt, float *Twa, float
               in++;
 
         return 0;
+}
+
+
+int velocitiesxt(float *ampa, float *dx, float *dz, float *dt, float *Twa, float Lwa, float h, int in)
+{
+
+  FILE * fp;
+
+  char name[FILENAME_MAX];
+    float z=0, x=0, t=0, k=0, tpi=0, a0=0, a1=0, a2=0, arg=0, kons=0, kons2=0, ratio=0, trig=0;
+    int lenght=0, i=0, i2=0;
+      snprintf(name, sizeof(name), "%dt.txt", in);
+      fp = fopen(name, "w");
+
+   lenght=Lwa/(*dx);
+   kons2=pi/2;
+   //We initialize the arrays dimensions to store the information on the wave velocities
+    float *arrayx = malloc(lenght * sizeof(*arrayx));
+    if (!arrayx) {
+        printf("There was a problem with malloc.");
+        exit(EXIT_FAILURE);
+    }
+    float *arrayy = malloc(lenght * sizeof(*arrayy));
+    if (!arrayy) {
+        printf("There was a problem with malloc.");
+        exit(EXIT_FAILURE);
+    }
+    //Constants to be used on the functions to calculate the velocities
+    tpi=2*pi;
+    a0=((tpi*(*ampa))/(*Twa));
+    k=(tpi)/Lwa;
+      //We start to calculate the velocities from t0 to tn=wave period
+        while(t<(*Twa))
+            {
+              a2=(t/(*Twa));
+              //We calculate the velocity from the mean water level z to the depth of propaation h
+                while(z<h)
+                    {
+                      kons=a0*cosh(k*(z-h))/sinh(k*h);
+                      i=0;
+                      //We calculate the velocity from the position x=0 to a xn=waves wavelenght
+                        while (x<Lwa)
+                            {
+
+                                    //if the wave field reach the bottom its velocity its calculated
+                                    arg=tpi*( -(x/Lwa) +a2 );
+                                    arrayx[i]=kons*sin(arg);
+                                    arrayy[i]= kons*cos(arg);
+
+                                  i++;
+                                  x=x+(*dx);
+                                }
+                                while(i2<=i){
+                                  //We store the whole data from the arrays to a file
+                                  fprintf(fp, "%.3f,%.3f\t", arrayx[i2], arrayy[i2]);
+                                  i2++;
+                                }
+                                fprintf(fp, "\n");
+                        i2=0;
+                        x=0;
+                        z=z+(*dz);
+                    }
+                    fprintf(fp, "\n\n");
+                    z=0;
+                    t=t+(*dt);
+            }
+              fclose(fp);
+              in++;
+
+        return 0;
+
+
+
 }
